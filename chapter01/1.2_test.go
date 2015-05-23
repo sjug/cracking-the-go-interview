ad6bcf9c6e6da2996ca6d925397d090222818f73
Sebastian Jug <sebastian.jug@gmail.com>
2015-05-22 20:43:47 -0400

    Question 2 completed.

--- chapter01/1.2_test.go
+++ chapter01/1.2_test.go
@@ -1,26 +1,34 @@
-package oneone
+package onetwo
 
 import "testing"
 
 var cases = []struct {
 	in   string
-	want bool
+	want string
 }{
 	{
 		"asdfghjkl",
-		true,
+		"lkjhgfdsa",
 	},
 	{
-		"qwertyuiopq",
-		false,
+		"qwertyuiop",
+		"poiuytrewq",
+	},
+	{
+		"1234567890",
+		"0987654321",
+	},
+	{
+		"The quick brown fox jumped over the frog.",
+		".gorf eht revo depmuj xof nworb kciuq ehT",
 	},
 }
 
 func TestIsUnique(t *testing.T) {
 	for _, tc := range cases {
-		got := IsUnique(tc.in)
+		got := Reverse(tc.in)
 		if got != tc.want {
-			t.Fatalf("IsUnique(%s) = %t want %t", tc.in, got, tc.want)
+			t.Fatalf("Reverse(%s) = %s want %s", tc.in, got, tc.want)
 		}
 	}
 	t.Log("Tested", len(cases), "cases.")
@@ -29,7 +37,7 @@ func TestIsUnique(t *testing.T) {
 func BenchmarkIsUnique(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for _, tc := range cases {
-			IsUnique(tc.in)
+			Reverse(tc.in)
 		}
 	}
 }
